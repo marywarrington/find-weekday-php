e2de791f27d2618f968b019bd95607f960624e95
Mary Warrington <marywarrington@gmail.com>
2016-02-18 10:42:54 -0800

    Add twig for form and results

--- app/app.php
+++ app/app.php
@@ -1,5 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Weekday.php";
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -9,6 +10,18 @@
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array(
             // twig input associative array
+            "form" => true
+        ));
+    });
+
+    $app->post("/results", function() use ($app) {
+        $my_Weekday = new Weekday;
+        $result = $my_Weekday->findDay($_POST['month'], $_POST['day'], $_POST['year']);
+        return $app['twig']->render('index.html.twig', array(
+            "form" => true, "message" => array(
+                'type' => 'info',
+                'text' => $result
+            )
         ));
     });
 
